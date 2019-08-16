4b193aa23fd940b66bf1b8a3f61a4249ce57baf0
tdronfield <t_dronfield@fanshaweonline.ca>
2019-08-16 17:05:41 -0400

    adding audio files and some js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,22 @@
 
 	//setting up our variables
 	const dropZones = document.querySelectorAll(".drop-zones"),
-		draggableIcons = document.querySelectorAll(".iconsContainer img");
+		draggableIcons = document.querySelectorAll(".iconsContainer img"),
+
+		//fireIcon = document.querySelector("#fire"),
+		//leavesIcon = document.querySelector("#leaves"),
+		// birdsIcon = document.querySelector("#birds"),
+		// radioIcon = document.querySelector("#radio"),
+		// waterIcon = document.querySelector("#water"),
+		// windIcon = document.querySelector("#wind"),
+		// thunderIcon = document.querySelector("#thunder"),
+		// cricketIcon = document.querySelector("#cricket");
+
+	//creates audio element for icons
+	// let fireIcon = document.createElement('audio');
+	// fireIcon.src = "audio/fire.mp3";
+	// fireIcon.load();
+
 
 	//We want to be able to drag the icons..
 	//So we add event listeners
@@ -31,6 +46,8 @@
 			//So we add the class "addColor" to the img of the targeted drop zone
 
 			this.classList.add("addColor");
+
+
 		});
 	});
 })();

204d3bdd88066b9db0b0d67b39056baea52be0b1
tdronfield <t_dronfield@fanshaweonline.ca>
2019-08-17 21:49:13 -0400

    adding fireZone.png and some js

--- js/main.js
+++ js/main.js
@@ -7,19 +7,58 @@
 	const dropZones = document.querySelectorAll(".drop-zones"),
 		draggableIcons = document.querySelectorAll(".iconsContainer img"),
 
-		//fireIcon = document.querySelector("#fire"),
-		//leavesIcon = document.querySelector("#leaves"),
-		// birdsIcon = document.querySelector("#birds"),
-		// radioIcon = document.querySelector("#radio"),
-		// waterIcon = document.querySelector("#water"),
-		// windIcon = document.querySelector("#wind"),
-		// thunderIcon = document.querySelector("#thunder"),
-		// cricketIcon = document.querySelector("#cricket");
+		//make variables for each icon
+		fireIcon = document.querySelector("#fire"),
+		leavesIcon = document.querySelector("#leaves"),
+		radioIcon = document.querySelector("#radio"),
+		waterIcon = document.querySelector("#water"),
+		windIcon = document.querySelector("#wind"),
+		thunderIcon = document.querySelector("#thunder"),
+		cricketIcon = document.querySelector("#cricket"),
+		birdsIcon = document.querySelector("#birds"),
+
+
+		//make variables for each drop zones
+		fireZone = document.querySelector(".bonfire"),
+		leavesZone = document.querySelector(".plants"),
+		radioZone = document.querySelector(".radio"),
+		waterZone = document.querySelector(".lake"),
+		windZone = document.querySelector(".flowers");
+
+
 
 	//creates audio element for icons
-	// let fireIcon = document.createElement('audio');
-	// fireIcon.src = "audio/fire.mp3";
-	// fireIcon.load();
+	let fireAudio = document.createElement('audio');
+	fireAudio.src = "audio/fire.mp3";
+	fireAudio.load();
+
+	// let leavesAudio = document.createElement('audio');
+	// leavesAudio.src = "audio/.mp3";
+	// leavesAudio.load();
+
+	// let radioAudio = document.createElement('audio');
+	// radioAudio.src = "audio/.mp3";
+	// radioAudio.load();
+
+	let waterAudio = document.createElement('audio');
+	waterAudio.src = "audio/water.mp3";
+	waterAudio.load();
+
+	let windAudio = document.createElement('audio');
+	windAudio.src = "audio/wind.mp3";
+	windAudio.load();
+
+	let thunderAudio = document.createElement('audio');
+	thunderAudio.src = "audio/thunder.mp3";
+	thunderAudio.load();
+
+	let cricketAudio = document.createElement('audio');
+	cricketAudio.src = "audio/crickets.mp3";
+	cricketAudio.load();
+
+	let birdsAudio = document.createElement('audio');
+	birdsAudio.src = "audio/birds.mp3";
+	birdsAudio.load();
 
 
 	//We want to be able to drag the icons..
